99249744c178c5b320f4b248f9fabc1895d6b26a
Lívia Rachel <83011310+Livia-Rachell@users.noreply.github.com>
2024-01-02 13:29:27 +0000

    inicia caixeiro

--- CaixeiroViajante.java
+++ CaixeiroViajante.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CaixeiroViajante {
     private Grafo grafoViajante;
 
     public CaixeiroViajante(Grafo grafo) {
         this.grafoViajante = grafo;
+    }
+
+    public void resolucao(Grafo grafo) {
+        agm(grafo);
+    }
+
+    public void agm(Grafo grafo) {
+        List<Aresta> arestasAGM = new ArrayList<Aresta>();
+        Collections.copy(arestasAGM, grafo.arestas());
+        arestasAGM.sort();
     }
 
 }
